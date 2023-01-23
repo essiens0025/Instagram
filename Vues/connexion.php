700397abd4979fa24ac10b8bd3b1e4aa99451604
essiens0025 <essiens0025@gmail.com>
2023-01-23 16:24:08 +0100

    first commit

---
+++ Vues/connexion.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../Utils/style.css">
+    <title>Se connecter</title>
+</head>
+
+<body>
+    <div>
+        <img class="icone" src="../Images/Instagram_icon.png.webp">
+    </div>
+    <div>
+        <h1 class="h1co">Se Connecter</h1>
+    </div>
+    <div>
+        <form class="form_co" action="" method="post">
+            Nom d'utilisateur: <input type="text" name="pseudo"><br>
+            Mot de Passe: <input type="text" name="password"><br>
+            <input style="margin-left : 370px ; font-size : 20px ; margin-top: 30px; margin-bottom: 15px; "
+                type="submit" value="Se connecter">
+        </form>
+    </div>
+    <a style="padding-left : 985px; font-size:20px" href="inscription.php">S'inscrire</a>
+</body>
+
+</html>
